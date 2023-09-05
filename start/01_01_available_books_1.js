b4e6c21f43eaa10fb23abcf7a33f86bfaa9d7fa6
showboatblastoff <124955423+showboatblastoff@users.noreply.github.com>
2023-09-05 18:52:36 +0000

    commit3

--- start/01_01_available_books_1.js
+++ start/01_01_available_books_1.js
@@ -14,29 +14,36 @@ Book.prototype.getAvailability = function() {
     return "Low Stock";
   }
   return "In Stock";
-  }
+}
 
 Book.prototype.sell = function(numCopiesSold = 1) {
   this.numCopies -= numCopiesSold;
 }
+
 Book.prototype.restock = function(numCopiesStocked = 5) {
   this.numCopies += numCopiesStocked;
 }
 
 
 
-const BookemDanno = new Book("Bookem Danno", "Spartan Dave", "101", 20);
-console.log(BookemDano.getAvailability());
+const SnookemDanno = new Book("Snookem Danno", "Spartan Dave", "101", 20);
+console.log(SnookemDano.getAvailability());
+
 const TigersForFree = new Book("Tigers For Free", "Tony Tigerman", "102", 7);
 console.log(TigersForFree.getAvailability());
+
 const WasntMeAgain = new Book("Wasn't Me, Again", "Crime Guy", "103", 15);
 console.log(WasntMeAgain.getAvailability());
+
 const TryThatIDareYou = new Book("Try That, I Dare You", "Sen EfBen", "104", 10);
 console.log(TryThatIDareYou.getAvailability());
+
 const ImNotMakingFaces = new Book("I'm Not Making Faces", "Giovanni Ribisi", "105", 10);
 console.log(ImNotMakingFaces.getAvailability());
+
 const DrumCircle = new Book("Drum Circle", "Tom Tomclub", "106", 10);
 console.log(DrumCircle.getAvailability());
+
 const NotSoFunkyMedina = new Book("Not So Funky Medina", "Tone Loc", "107", 10);
 console.log(NotSoFunkyMedina.getAvailability());
 
