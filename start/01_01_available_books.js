4039b2d3911ac40d2c27a8efc0e9a42eac98249e
showboatblastoff <124955423+showboatblastoff@users.noreply.github.com>
2023-09-05 18:05:18 +0000

    library first trh

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,18 @@
 // Write your code here
+class Books {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+
+  }
+}
+
+const book101 = new Book("Bookem Danno", "Spartan Dave", "101", 10);
+const book102 = new Book("Tigers For Free", "Tony Tigerman", "102", 10);
+const book103 = new Book("Wasn't Me, Again", "Crime Guy", "103", 10);
+const book104 = new Book("Try That, I Dare You", "Sen EfBen", "104", 10);
+const book105 = new Book("I'm Not Making Faces", "Giovanni Ribisi", "105", 10);
+const book106 = new Book("Drum Circl", "Tom Tomclub", "106", 10);
+const book107 = new Book("Not So Funky Medina", "Tone Loc", "107", 10);
