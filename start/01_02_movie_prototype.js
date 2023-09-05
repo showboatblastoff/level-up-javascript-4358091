9adb7b05ff3a0c00287b898137d45009afa0b983
showboatblastoff <124955423+showboatblastoff@users.noreply.github.com>
2023-09-05 23:14:53 +0000

    commit9

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,5 +1,5 @@
 // Write your code here
-class film {
+class Film {
   constructor(title, director, genre, releaseYear, rating) {
     this.title = title;
     this.director = director;
@@ -7,7 +7,17 @@ class film {
     this.releaseYear = releaseYear;
     this.rating = rating;
   }
+
+  get overview() {
+    return this.getOverview();
+  }
+
   getOverview() {
-    return `${this.title} , a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${rating}.`;
+    return `${this.title} , a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
   }
 }
+
+const MoviePhone = new Film("Movie Phone","Johnny Knoxville", "horror", 2021, 4)
+const Kelp = new Film("Kelp","Water Beatleman", "comedy", 1973, 10)
+const Nerd = new Film("Nerd","Guillermo DelToro", "drama", 2006, 3)
+const Oopsie = new Film("Oopsie","Bean Lunchman", "horror", 2021, 4)
